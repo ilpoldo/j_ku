06addc2f96f678e2938cf375e6ebe7cba656be7d
Leandro Pedroni <ilpoldo@gmail.com>
2009-04-26 02:01:40 +0200

    Added compression tools

---
+++ compressor/compress.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+abspath="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
+dir=`dirname "$abspath"`
+echo $dir
+java -jar $dir/yuicompressor-2.4.2.jar --nomunge $dir/../jquery.j_ku.js -o $dir/../jquery.j_ku-min.js
